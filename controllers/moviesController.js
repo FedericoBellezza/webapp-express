9c65ba83c6afa631101aa3c7a02d481c51163844
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-01-10 16:57:42 +0100

    add show route

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -3,12 +3,41 @@ const connection = require("../db/conn");
 // index
 function index(req, res) {
   connection.query("SELECT * FROM movies", (err, result) => {
-    console.log(result);
-  });
+    // if error
+    console.log(err);
+    if (err) return res.status(500).json({ error: "Database query failed" });
 
-  res.json({
-    message: "index",
+    res.json(result);
   });
 }
 
-module.exports = { index };
+// show
+function show(req, res) {
+  // query preparation
+  getMovieFromID = "SELECT * FROM movies WHERE id = ?";
+  getReviewsFromID = "SELECT * FROM movies.reviews WHERE movie_id = ?";
+  id = req.params.id;
+  finalRes = [];
+
+  // get movie from id
+  connection.query(getMovieFromID, [id], (err, result) => {
+    // if error
+    console.log(err);
+    if (err) return res.status(500).json({ error: "Database query failed" });
+
+    finalRes.push({ movie: result });
+  });
+
+  // get reviews from id
+  connection.query(getReviewsFromID, [id], (err, result) => {
+    // if error
+    console.log(err);
+    if (err) return res.status(500).json({ error: "Database query failed" });
+
+    finalRes.push({ reviews: result });
+    res.json(finalRes);
+    console.log(finalRes);
+  });
+}
+
+module.exports = { index, show };
--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // import from controllers
 const moviesController = require("../controllers/moviesController");
 router.get("/", moviesController.index);
+router.get("/:id", moviesController.show);
 
 // router export
 module.exports = router;
